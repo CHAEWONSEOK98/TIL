3e2e99ff6147c065b28a9a7862ffc45169f437d1
CHAEWONSEOK98 <7dugod7@naver.com>
2023-07-23 23:16:48 +0900

    object & key value mapping

--- TypeScript/8_object/object.ts
+++ TypeScript/8_object/object.ts
@@ -218,3 +218,72 @@ const companyOrPet: CompanyOrPet = {
   petName: '킹캣',
   petAge: 3,
 };
+
+// [Key Value Mapping]
+
+enum State {
+  loading,
+  done,
+  error,
+}
+
+type GlobalApiStatus = {
+  getUser: State;
+  paginateUsers: State | undefined;
+  banUser: State | null;
+  getPosts: State;
+};
+
+type UserApiStatus = {
+  getUser: State;
+  paginateUsers: State | undefined;
+  banUser: State | null;
+};
+
+type UserApiStatus2 = {
+  getUser: GlobalApiStatus['getUser'];
+  paginateUsers: GlobalApiStatus['paginateUsers'];
+  banUser: GlobalApiStatus['banUser'];
+};
+
+type UserApiStatus3 = {
+  [k in 'getUser' | 'paginateUsers' | 'banUser']: GlobalApiStatus[k];
+};
+
+type PickedUserApiStatus = Pick<
+  GlobalApiStatus,
+  'getUser' | 'banUser' | 'paginateUsers'
+>;
+
+type OmitUserApiStatus = Omit<GlobalApiStatus, 'getPosts'>;
+
+// [Key Value Mapping] - keyof
+
+const key: AllKeys = 'getUser';
+
+type KeyOfUserApiStatus = {
+  [k in keyof GlobalApiStatus]: GlobalApiStatus[k];
+};
+
+type KeyOfUserApiStatus2 = {
+  [k in Exclude<keyof GlobalApiStatus, 'getPosts'>]: GlobalApiStatus[k];
+};
+
+type KeyOfUserApiStatus3 = {
+  [k in Exclude<keyof GlobalApiStatus, 'getPosts'>]?: GlobalApiStatus[k];
+};
+
+//
+interface LoadingStatus {
+  type: 'loading';
+  data: string[];
+}
+
+interface ErrorStatus {
+  type: 'error';
+  message: string;
+}
+
+type FetchStatus = LoadingStatus | ErrorStatus;
+
+type StatusType = FetchStatus['type']; // 'loading' | 'error' type
